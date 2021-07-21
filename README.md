586577643a79eb138b3e84e7d672a1aab876ef47
spiltcoffee <spiltcoffee@users.noreply.github.com>
2021-07-21 17:39:39 +1000

    chore: added proper README; added GPLv3 Lesser license

---
+++ README.md
@@ -0,0 +1,11 @@
+# asteroids
+
+This is a clone of Asteroids that I made in early 2013 as part of a University course. 
+
+To toot my own horn for a moment, it earned me top of the class, and in the following year, top prize in a game development competition - which included a trophy and a sweet $700 AUD.
+
+No guarantees that it still compiles just yet - I'll eventually get around to establishing a release process for the game.\
+<small><small>Honestly, not even sure it works on Windows 10...</small></small>
+
+Enjoy!\
+\- SpiltCoffee
--- src/asCollisions.pas
+++ src/asCollisions.pas
@@ -1,3 +1,21 @@
+{
+    SwinGame Asteroids
+    Copyright (C) 2013  SpiltCoffee
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+}
+
 unit asCollisions;
 
 interface
@@ -204,8 +222,3 @@ implementation
   end;
 
 end.
-
-//______________________________________________________//
-//                                                      //
-// SwinGame Asteroids - Copyright SpiltCoffee 2010-2013 //
-//______________________________________________________//
